42095bfd5d2e04653b70f0252b4d00fc6e4de3e3
Param Savjani <paramsavjnai3010@gmail.com>
2024-10-22 20:53:57 +0530

    nothing more

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ const deleteAllUsers = async () => {
 
 connectDB()
   .then(() => {
-    app.listen(process.env.PORT || 3000, () => {
+    app.listen(process.env.PORT || 2422, () => {
       console.log(`Server is running on port ${process.env.PORT}`);
       // deleteAllUsers();
     });
