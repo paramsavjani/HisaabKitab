8a915b3a0406e521f39ffaed06c0343a2ae8c480
Param Savjani <paramsavjnai3010@gmail.com>
2024-10-22 17:57:09 +0530

    add deleteFriend function

--- src/controllers/friend.controller.js
+++ src/controllers/friend.controller.js
@@ -29,10 +29,41 @@ const getAllFriends = asyncHandler(async (req, res) => {
     })
   );
 
-  console.log(friends);
-
   const response = new ApiResponse(200, friends, "All friends");
   res.status(response.statusCode).json(response);
 });
 
-export { getAllFriends };
+const deleteFriend = asyncHandler(async (req, res) => {
+  const username = req.user.username;
+  const friendUsername = req.params.username;
+
+  const user = await User.findOne({ username }).select("_id");
+
+  if (!user) {
+    return res.status(404).json({ message: "User not found" });
+  }
+
+  const friend = await User.findOne({ username: friendUsername }).select("_id");
+
+  if (!friend) {
+    return res.status(404).json({ message: "Friend not found" });
+  }
+
+  const friendShip = await Friend.findOne({
+    $or: [
+      { userId: user._id, friendId: friend._id },
+      { userId: friend._id, friendId: user._id },
+    ],
+  });
+
+  if (!friendShip) {
+    return res.status(404).json({ message: "Friendship not found" });
+  }
+
+  await Friend.findByIdAndDelete(friendShip._id);
+
+  const response = new ApiResponse(200, {}, "Friend removed");
+  res.status(response.statusCode).json(response);
+});
+
+export { getAllFriends, deleteFriend };
--- src/routes/friend.route.js
+++ src/routes/friend.route.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { verifyJWT } from "../middleware/auth.middleware.js";
-import { getAllFriends } from "../controllers/friend.controller.js";
+import { getAllFriends,deleteFriend } from "../controllers/friend.controller.js";
 
 const router = Router();
 
 router.route("/").get(verifyJWT, getAllFriends);
 
+router.route("/:username/delete").delete(verifyJWT, deleteFriend);
 
 
 export default router;
