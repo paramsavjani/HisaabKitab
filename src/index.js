c403b1aef729f4203e828bd94375e13dbc1a2897
= <=>
2024-12-14 02:07:57 +0530

    Add NotFound component and update routing; change title to 'Cash Track'

--- src/index.js
+++ src/index.js
@@ -11,12 +11,27 @@ import {
   Route,
   RouterProvider,
 } from "react-router-dom";
+import NotFound from "./Components/NotFound";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Layout />}>
+      <Route
+        path=""
+        element={
+          <div className="min-h-screen flex items-center justify-center bg-gray-900 text-cyan-50">
+            this is the home page
+          </div>
+        }
+      />
       <Route path="login" element={<Login />} />
       <Route path="signup" element={<Signup />} />
+      <Route
+        path="*"
+        element={
+          <NotFound />
+        }
+      />
     </Route>
   )
 );
