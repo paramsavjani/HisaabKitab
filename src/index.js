3e6087531c655a6dfb10d164d824f411152cc005
= <=>
2024-12-15 01:52:10 +0530

    refactor: use environment variable for server port configuration

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 dotenv.config({ path: "../.env" });
 import connectDB from "./db/index.js";
 import { app } from "./app.js";
-const PORT = 1000;
+const PORT = process.env.PORT || 1000;
 
 connectDB()
   .then(() => {
