eb507aab046f5055281ec33f789eda6b11fd5e08
Param Savjani <paramsavjani3010@gmail.com>
2024-12-24 00:31:42 +0530

    Update backend URL in environment variables and configure socket connection to use WebSocket transport

--- src/socket.js
+++ src/socket.js
@@ -1,12 +1,13 @@
 import { Preferences } from "@capacitor/preferences";
 import { io } from "socket.io-client";
 
-const socket = io(process.env.REACT_APP_BACKEND_URL, {
+const socket = io("wss://backend-for-khatabook-f1cr.onrender.com", {
   auth: async (cb) => {
     cb({
       accessToken: (await Preferences.get({ key: "accessToken" })).value,
     });
   },
+  transports: ["websocket"],
   reconnection: true,
   reconnectionAttempts: 5,
   reconnectionDelay: 1000,
