f5a5b3566cc3e4833409d8d165dadb6a229c93bf
Param Savjani <paramsavjani3010@gmail.com>
2024-12-24 23:19:07 +0530

    Update socket connection to handle web platform correctly

--- src/socket.js
+++ src/socket.js
@@ -1,19 +1,25 @@
 import { Preferences } from "@capacitor/preferences";
 import { io } from "socket.io-client";
+import { Capacitor } from "@capacitor/core";
 
-const socket = io(`wss://${process.env.REACT_APP_BACKEND_URL.split("//")[1]}`, {
-  auth: async (cb) => {
-    const { value: storedAccessToken } = await Preferences.get({
-      key: "accessToken",
-    });
-    cb({
-      accessToken: storedAccessToken,
-    });
-  },
-  transports: ["websocket"],
-  reconnection: true,
-  reconnectionAttempts: 5,
-  reconnectionDelay: 1000,
-});
+const socket = io(
+  Capacitor.getPlatform() === "web"
+    ? process.env.REACT_APP_BACKEND_URL
+    : `wss://${process.env.REACT_APP_BACKEND_URL.split("//")[1]}`,
+  {
+    auth: async (cb) => {
+      const { value: storedAccessToken } = await Preferences.get({
+        key: "accessToken",
+      });
+      cb({
+        accessToken: storedAccessToken,
+      });
+    },
+    transports: ["websocket"],
+    reconnection: true,
+    reconnectionAttempts: 5,
+    reconnectionDelay: 1000,
+  }
+);
 
 export default socket;
