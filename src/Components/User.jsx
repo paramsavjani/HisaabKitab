21a42d21d35b93019ca17c7c5d71094ab35ef343
Param Savjani <paramsavjani3010@gmail.com>
2024-12-25 22:19:10 +0530

    Refactor User component for improved layout and styling; update user details display

--- src/Components/User.jsx
+++ src/Components/User.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState, useContext } from "react";
 import { Link, useParams } from "react-router-dom";
 import { FaSpinner } from "react-icons/fa";
-import { toast, ToastContainer } from "react-toastify";
+import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import UserContext from "../context/UserContext.js";
 import UserNotFound from "./UserNotFound";
@@ -178,46 +178,158 @@ const User = () => {
     return <UserNotFound />;
   }
 
+  // return (
+  //   <>
+  //     <div className="min-h-screen bg-black text-white flex items-center justify-center px-4 sm:px-6 lg:px-8 py-8">
+  //       <div className="bg-gray-800 p-6 sm:p-8 rounded-lg shadow-lg w-full max-w-md sm:max-w-lg lg:max-w-xl space-y-6">
+  //         <div className="flex flex-col sm:flex-row items-center sm:space-x-6 space-y-4 sm:space-y-0">
+  //           <img
+  //             src={
+  //               profile.profilePicture ||
+  //               "https://tse1.mm.bing.net/th/id/OIP.aYhGylaZyL4Dj0CIenZPlAHaHa?rs=1&pid=ImgDetMain"
+  //             }
+  //             alt={profile.username}
+  //             className="w-24 h-24 sm:w-32 sm:h-32 rounded-full object-cover border-4 border-gray-500 flex-shrink-0"
+  //           />
+  //           <div className="text-center sm:text-left flex-1">
+  //             <h1 className="text-2xl sm:text-3xl font-semibold truncate">
+  //               {profile.name || "No Name"}
+  //             </h1>
+  //             <p className="text-green-400 truncate">@{profile.username}</p>
+  //             <p className="text-gray-400 truncate">{profile.email}</p>
+  //             <p className="text-gray-400 truncate">
+  //               Total Friends: {profile.totalFriends || 0}
+  //             </p>
+  //           </div>
+  //         </div>
+  //         <div className="mt-4">
+  //           <h3 className="text-lg sm:text-xl font-semibold text-gray-300">
+  //             User Details
+  //           </h3>
+  //           <ul className="mt-4 space-y-3 text-gray-400">
+  //             {[
+  //               { label: "Name", value: profile.name || "Not Provided" },
+  //               { label: "Username", value: profile.username },
+  //               { label: "Email", value: profile.email },
+  //               { label: "Total Friends", value: profile.totalFriends || 0 },
+  //             ].map((item, index) => (
+  //               <li key={index}>
+  //                 <span className="font-medium text-white">{item.label}:</span>{" "}
+  //                 {item.value}
+  //               </li>
+  //             ))}
+  //           </ul>
+  //         </div>
+  //         {user && user.username !== profile.username && (
+  //           <div className="mt-6 space-y-4">
+  //             {!isFriend &&
+  //               !isRequestSent &&
+  //               !isRequestReceived &&
+  //               renderButton(
+  //                 "Add Friend",
+  //                 addFriend,
+  //                 isAddingFriend,
+  //                 "bg-green-600 text-white focus:ring-green-500",
+  //                 "Adding..."
+  //               )}
+
+  //             {!isFriend &&
+  //               isRequestSent &&
+  //               renderButton(
+  //                 "Cancel Request",
+  //                 cancelFriendRequest,
+  //                 isCancelingRequest,
+  //                 "bg-red-600 text-white focus:ring-red-500",
+  //                 "Canceling..."
+  //               )}
+
+  //             {isRequestReceived && (
+  //               <div className="flex space-x-4">
+  //                 {renderButton(
+  //                   "Accept",
+  //                   () => handleRequest(requestId, "accept"),
+  //                   isAccepting,
+  //                   "bg-green-500 text-white",
+  //                   "Accepting..."
+  //                 )}
+  //                 {renderButton(
+  //                   "Reject",
+  //                   () => handleRequest(requestId, "deny"),
+  //                   isRejecting,
+  //                   "bg-red-500 text-white",
+  //                   "Rejecting..."
+  //                 )}
+  //               </div>
+  //             )}
+
+  //             {isFriend && (
+  //               <Link
+  //                 to={`/transactions/${user.username}--${profile.username}`}
+  //                 className="w-full px-4 py-3 bg-green-500 text-white text-lg font-semibold rounded-lg text-center block"
+  //               >
+  //                 Transaction
+  //               </Link>
+  //             )}
+  //           </div>
+  //         )}
+  //       </div>
+  //     </div>
+  //     <ToastContainer
+  //       position="top-right"
+  //       autoClose={3000}
+  //       limit={5}
+  //       hideProgressBar={false}
+  //       newestOnTop={false}
+  //       closeOnClick
+  //       rtl={false}
+  //       pauseOnFocusLoss={false}
+  //       draggable
+  //       pauseOnHover
+  //       theme="colored"
+  //     />
+  //   </>
+  // );
+
   return (
-    <>
-      <div className="min-h-screen bg-black text-white flex items-center justify-center px-4 sm:px-6 lg:px-8 py-8">
-        <div className="bg-gray-800 p-6 sm:p-8 rounded-lg shadow-lg w-full max-w-md sm:max-w-lg lg:max-w-xl space-y-6">
-          <div className="flex flex-col sm:flex-row items-center sm:space-x-6 space-y-4 sm:space-y-0">
+    <div className="min-h-screen flex items-center justify-center bg-black text-white px-4">
+      <div className="w-full max-w-xs p-6 rounded-xl shadow-lg bg-gray-900">
+        {/* Header Section */}
+        <div className="flex items-center mb-6">
+          {/* User Image */}
+          <div className="w-24 h-24 bg-gray-700 rounded-full flex items-center justify-center overflow-hidden mr-6">
             <img
               src={
                 profile.profilePicture ||
                 "https://tse1.mm.bing.net/th/id/OIP.aYhGylaZyL4Dj0CIenZPlAHaHa?rs=1&pid=ImgDetMain"
-              }
-              alt={profile.username}
-              className="w-24 h-24 sm:w-32 sm:h-32 rounded-full object-cover border-4 border-gray-500 flex-shrink-0"
+              } // fallback image if no profile picture is available
+              alt="Profile"
+              className="w-full h-full object-cover"
             />
-            <div className="text-center sm:text-left flex-1">
-              <h1 className="text-2xl sm:text-3xl font-semibold truncate">
-                {profile.name || "No Name"}
-              </h1>
-              <p className="text-green-400 truncate">@{profile.username}</p>
-              <p className="text-gray-400 truncate">{profile.email}</p>
-            </div>
           </div>
-          <div className="mt-4">
-            <h3 className="text-lg sm:text-xl font-semibold text-gray-300">
-              User Details
-            </h3>
-            <ul className="mt-4 space-y-3 text-gray-400">
-              <li>
-                <span className="font-medium text-white">Name:</span>{" "}
-                {profile.name || "Not Provided"}
-              </li>
-              <li>
-                <span className="font-medium text-white">Username:</span>{" "}
-                {profile.username}
-              </li>
-              <li>
-                <span className="font-medium text-white">Email:</span>{" "}
-                {profile.email}
-              </li>
-            </ul>
+
+          {/* User Details */}
+          <div>
+            <p className="text-xl font-bold">{profile.name || "No Name"}</p>
+            <h1 className="text-sm  text-green-500">@{profile.username}</h1>
           </div>
+        </div>
+
+        {/* Divider */}
+        <div className="border-t border-gray-700 mb-4"></div>
+
+        {/* Info Section */}
+        <div className="space-y-4">
+          {/* Friends Count */}
+          <div className="flex justify-between items-center bg-gray-800 p-4 rounded-lg">
+            <p className="text-sm font-medium">Total Friends</p>
+            <p className="text-lg font-semibold text-gray-200">
+              {profile.totalFriends || 0}
+            </p>
+          </div>
+        </div>
+
+        {/* Footer Button */}
+        <div className="mt-6">
           {user && user.username !== profile.username && (
             <div className="mt-6 space-y-4">
               {!isFriend &&
@@ -265,27 +377,14 @@ const User = () => {
                   to={`/transactions/${user.username}--${profile.username}`}
                   className="w-full px-4 py-3 bg-green-500 text-white text-lg font-semibold rounded-lg text-center block"
                 >
-                  Transaction
+                  Transactions
                 </Link>
               )}
             </div>
           )}
         </div>
       </div>
-      <ToastContainer
-        position="top-right"
-        autoClose={3000}
-        limit={5}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss={false}
-        draggable
-        pauseOnHover
-        theme="colored"
-      />
-    </>
+    </div>
   );
 };
 
